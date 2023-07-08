72579d43e91d08e8f581d301e02f99ee4e227d40
frontendparham <tavakolianparham@gmail.com>
2023-07-08 15:11:29 +0330

    Configure React app for deployment to GitHub Pages

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./scss/app.scss";
 import App from "./components/App";
+import BrowserRouter from "react-router-dom/BrowserRouter";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
